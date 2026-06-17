4f49dba69935a8694438ef6d8e96e99a4e8d45d3
Shruthi Kallimath <77193241+shruthi-kallimath@users.noreply.github.com>
2026-06-18 00:33:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Hello from Shruthi DevOps Project 🚀"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
